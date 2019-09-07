d362fb5ac88d0f8abcf142185c63eace13aa81b2
Ulises Ancona <uliancona@hotmail.com>
2019-09-07 17:10:39 -0500

    cambio al titulo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    BLOGII
+                    BLOGII uli
                 </div>
 
                 <div class="links">
